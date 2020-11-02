556d37d03db8be3b62ee12daa28743e069789c66
mbertis <madisonbertis@gmail.com>
2020-11-02 11:06:23 -0800

    added comments to scripts file and formatted with Prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,86 @@
 (function () {
-    let form = document.querySelector("#contact-form");
-    let emailInput = document.querySelector("#contact-email");
-    let phoneInput = document.querySelector("#contact-tel");
-    let messageInput = document.querySelector("#contact-message");
+  let form = document.querySelector("#contact-form");
+  let emailInput = document.querySelector("#contact-email");
+  let phoneInput = document.querySelector("#contact-tel");
+  let messageInput = document.querySelector("#contact-message");
 
-function validateEmail() {
+  function validateEmail() {
     let value = emailInput.value;
 
     if (!value) {
-        showErrorMessage(emailInput, "Email is required");
-        return false;
+      showErrorMessage(emailInput, "Email is required");
+      return false;
     }
 
+    if (value.indexOf("@") === -1) {
+      showErrorMessage(emailInput, "Please enter a valid email address");
+      return false;
+    }
 
-if (value.indexOf("@") === -1) {
-    showErrorMessage(emailInput, "Please enter a valid email address");
-    return false;
-}
+    showErrorMessage(emailInput, null);
+    return true;
+  }
 
-showErrorMessage(emailInput, null);
-return true;
-}
-
-function validateTelephone() {
+  function validateTelephone() {
     let value = phoneInput.value;
 
-    if(!value) {
-        showErrorMessage(phoneInput, "Phone number is required");
-        return false;
+    if (!value) {
+      showErrorMessage(phoneInput, "Phone number is required");
+      return false;
     }
 
-    if(value.length < 10) {
-        showErrorMessage(phoneInput, "Phone number must include US area code");
-        return false;
+    if (value.length < 10) {
+      showErrorMessage(phoneInput, "Phone number must include US area code");
+      return false;
     }
 
     showErrorMessage(phoneInput, null);
     return true;
-}
+  }
 
-function validateMessage() {
+  function validateMessage() {
     let value = messageInput.value;
 
-    if(!value) {
-        showErrorMessage(messageInput, "Message is required");
-        return false;
+    if (!value) {
+      showErrorMessage(messageInput, "Message is required");
+      return false;
     }
 
     showErrorMessage(messageInput, null);
     return true;
-}
+  }
 
-function showErrorMessage(input, message) {
-    let container = input.parentElement;
-    let error = container.querySelector(".error-message");
-    if(error) {
-        container.removeChild(error);
+  function showErrorMessage(input, message) {
+    let container = input.parentElement; // the .input-wrapper
+    let error = container.querySelector(".error-message"); // removes existing error message and replaces with new
+    if (error) {
+      container.removeChild(error);
     }
-    if(message) {
-        let error = document.createElement("div");
-        error.classList.add("error-message");
-        error.innerText = message;
-        container.appendChild(error);
+    if (message) {
+      let error = document.createElement("div");
+      error.classList.add("error-message");
+      error.innerText = message;
+      container.appendChild(error);
     }
-}
+  }
 
-function validateForm() {
+  function validateForm() {
+    // prevents form from being submitted if other validations are not true
     let isValidEmail = validateEmail();
     let isValidPhone = validateTelephone();
     let isValidMessage = validateMessage();
     return isValidEmail && isValidPhone && isValidMessage;
-}
+  }
 
-form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", (e) => {
+    // creates pop-up alert if form is submitted successfully
     e.preventDefault();
-    if(validateForm()) {
-        alert("Message Sent!");
+    if (validateForm()) {
+      alert("Message Sent!");
     }
-});
+  });
 
-emailInput.addEventListener("input", validateEmail);
-phoneInput.addEventListener("input", validateTelephone);
-messageInput.addEventListener("input", validateMessage);
-
+  emailInput.addEventListener("input", validateEmail); // checks for validation as uers begins typing
+  phoneInput.addEventListener("input", validateTelephone);
+  messageInput.addEventListener("input", validateMessage);
 })();
